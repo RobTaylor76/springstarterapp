306cd46cf09561a6d48d915dda3c338d21fc0faf
Rob Taylor <Robert_taylor_@hotmail.com>
2014-05-03 19:03:19 +0100

    working config

--- src/test/java/com/miniaturesolutions/TestContext.java
+++ src/test/java/com/miniaturesolutions/TestContext.java
@@ -8,12 +8,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"applicationContext.xml"})
+@ContextConfiguration(locations={"/spring-config.xml"})
 public class TestContext {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
